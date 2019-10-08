9d5b77cbf438e78756ed43290fcae3178180c709
Marius <marius000steger@gmail.com>
2019-10-08 16:48:38 +0200

    reformat code

--- server/sensors/moisture_sensors.py
+++ server/sensors/moisture_sensors.py
@@ -3,46 +3,50 @@ import time
 import threading
 import datetime
 
+
 class sensor(object):
 
-    def __init__(self, add = 0x48, port_in = 0x02):
+    def __init__(self, add=0x48, port_in=0x02):
         self.i2c_address = add  # I2C-Adresse des Wandler
         self.analog_port_in = port_in  # Sensor-Standard
         self.i2c = smbus.SMBus(1)  # 2C-Instanz
 
     def readI2c(self):
-        _ = self.i2c.read_byte_data(self.i2c_address, self.analog_port_in)  # placeholder und neuen Wert generieren aus Speicher
+        _ = self.i2c.read_byte_data(self.i2c_address,
+                                    self.analog_port_in)  # placeholder und neuen Wert generieren aus Speicher
         time.sleep(0.01)
-        val = self.i2c.read_byte_data(self.i2c_address, self.analog_port_in)  # aktueller Wert aus Speicher - zeitpunkt erste Abfrage
+        val = self.i2c.read_byte_data(self.i2c_address,
+                                      self.analog_port_in)  # aktueller Wert aus Speicher - zeitpunkt erste Abfrage
         return val
 
     def writeI2c(self, i2cDeviceAddr, channel, value):
-    #    self.i2c.write_byte_data(i2cDeviceAddr, 0x40 + channel, value)
+        #    self.i2c.write_byte_data(i2cDeviceAddr, 0x40 + channel, value)
         print("not implemented yet")
+
 
 class ThreadedSensor(object):
 
     def __init__(self, intervall=1):
         self.interval = intervall
-	self._stop = False
+        self._stop = False
 
     def start(self, logpath):
-	self.logpath = logpath
+        self.logpath = logpath
         thread = threading.Thread(target=self.run, args=())
-        thread.daemon = True                            # Daemonize thread
-        thread.start()                                  # Start the execution
+        thread.daemon = True  # Daemonize thread
+        thread.start()  # Start the execution
 
     def stop(self):
         self._stop = True
 
     def run(self):
         """ Method that runs forever """
-	s = sensor()
+        s = sensor()
         while not self._stop == True:
             _ = s.readI2c()
             val = s.readI2c()
-	    with open(self.logpath, 'a') as file:
-		file.write(datetime.datetime.now().strftime("%H:%M:%S")+', '+str(val)+' \n')
-	    time.sleep(self.interval)
+            with open(self.logpath, 'a') as file:
+                file.write(datetime.datetime.now().strftime("%H:%M:%S") + ', ' + str(val) + ' \n')
+            time.sleep(self.interval)
         with open(self.logpath, 'a') as file:
-                file.write(datetime.datetime.now().strftime("%H:%M:%S")+', '+ 'closed' +' \n')
+            file.write(datetime.datetime.now().strftime("%H:%M:%S") + ', ' + 'closed' + ' \n')
--- server/test_template.py
+++ server/test_template.py
@@ -7,51 +7,53 @@ import os
 
 log_name = "default.txt"
 app = Flask(__name__)
-Bootstrap (app)
+Bootstrap(app)
 moistureString = "?"
 running = 0
 
+
 @app.route("/")
 def index():
-   sensor = moisture_sensors.sensor()
-   moistureString = str(sensor.readI2c())
-   templateData = {
-      'title' : 'moisture sensor',
-      'time': datetime.datetime.now().strftime("%Y-%m-%d %H:%M"),
-      'moisture': moistureString,
-      'running': running
-      }
-   del sensor
-   return render_template('index.html', **templateData)
+    sensor = moisture_sensors.sensor()
+    moistureString = str(sensor.readI2c())
+    templateData = {
+        'title': 'moisture sensor',
+        'time': datetime.datetime.now().strftime("%Y-%m-%d %H:%M"),
+        'moisture': moistureString,
+        'running': running
+    }
+    del sensor
+    return render_template('index.html', **templateData)
+
 
 @app.route("/ms/<action>")
 def logAction(action):
+    logPath = os.path.join("/home/marius/smart_garden/logs/", log_name)
 
-   logPath = os.path.join("/home/marius/smart_garden/logs/", log_name)
+    if action == "start":
+        try:
+            t.start(logPath)
+            running = 1
+        except:
+            running = 0
 
-   if action == "start":
-      try:
-         t.start(logPath)
-         running = 1
-      except:
-         running = 0
+    elif action == "stop":
+        try:
+            t.stop()
+            running = 0
+        except:
+            running = 1
 
-   elif action == "stop":
-      try:
-         t.stop()
-         running = 0
-      except:
-         running = 1
+    templateData = {
+        'title': 'moisture sensor',
+        'time': datetime.datetime.now().strftime("%Y-%m-%d %H:%M"),
+        'moisture': moistureString,
+        'running': running
+    }
 
-   templateData = {
-      'title' : 'moisture sensor',
-      'time': datetime.datetime.now().strftime("%Y-%m-%d %H:%M"),
-      'moisture': moistureString,
-      'running': running
-      }
+    return render_template('index.html', **templateData)
 
-   return render_template('index.html', **templateData)
 
 if __name__ == "__main__":
-   t = moisture_sensors.ThreadedSensor()
-   app.run(host='0.0.0.0', port=1080, debug=True)
+    t = moisture_sensors.ThreadedSensor()
+    app.run(host='0.0.0.0', port=1080, debug=True)
